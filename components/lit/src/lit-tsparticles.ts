fb0cccb6b33dd5537b376b802c717ebff2cf0ea8
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-03-06 02:20:23 +0100

    build: working on particles component

--- components/lit/src/lit-tsparticles.ts
+++ components/lit/src/lit-tsparticles.ts
@@ -1,5 +1,6 @@
 import { LitElement, html } from "lit";
 import { property, customElement } from "lit/decorators.js";
+import { Container, Engine, tsParticles } from "tsparticles-engine";
 
 /**
  * The LitParticles element.
@@ -13,6 +14,44 @@ export class LitParticles extends LitElement {
    */
   @property({ type: String })
   id = "tsparticles";
+
+  /**
+   * The options
+   */
+  @property({ type: Object })
+  options = {};
+
+  container?: Container;
+
+  @property({ type: Function })
+  particlesInit?: (engine: Engine) => Promise<void>;
+
+  @property({ type: Function })
+  particlesLoaded?: (container?: Container) => Promise<void>;
+
+  constructor() {
+    super();
+
+    this.particlesInit?.(tsParticles);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    tsParticles.load(this.id, this.options).then(async (container) => {
+      this.container = container;
+
+      await this.particlesLoaded?.(container);
+    });
+  }
+
+  disconnectedCallback(): void {
+    if (this.container) {
+      this.container.destroy();
+    }
+
+    super.disconnectedCallback();
+  }
 
   render() {
     return html`<div id=${this.id}></div>`;
